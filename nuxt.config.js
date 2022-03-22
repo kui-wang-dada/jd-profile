315ab30f34dec8e72d89e961502e2199ffa2bd2c
wangkui <872505550@qq.com>
2022-03-22 21:06:57 +0800

    打包部署

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,15 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
+  router: {
+    base: '/',
+  },
+  render: {
+    resourceHints: false,
+  },
+  generate: {
+    dir: 'jd-profile',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
