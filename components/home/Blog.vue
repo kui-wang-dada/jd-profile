48613e06032032933f67edb2c8258bd90f780e21
wangkui <872505550@qq.com>
2022-03-28 09:10:53 +0800

    博客

--- components/home/Blog.vue
+++ components/home/Blog.vue
@@ -10,7 +10,7 @@
         <h4>个人经历经验</h4>
         <div class="list-wrap">
           <div v-for="item in list1" :key="item.title" class="list-item">
-            <h3>title1</h3>
+            <a :href="item.link">{{ item.title }}</a>
           </div>
         </div>
       </div>
@@ -18,7 +18,7 @@
         <h4>技术文章笔记</h4>
         <div class="list-wrap">
           <div v-for="item in list2" :key="item.title" class="list-item">
-            <h3>title2</h3>
+            <a :href="item.link">{{ item.title }}</a>
           </div>
         </div>
       </div>
@@ -34,78 +34,34 @@ export default {
     return {
       list1: [
         {
-          title: '',
-          link: '',
+          title: '27岁转行风雨路——那年毕业，不知换了多少工作...',
+          link: 'https://juejin.cn/post/6961010353471750158',
         },
         {
-          title: '',
-          link: '',
+          title: '27岁转行风雨路2——前端跳槽之路',
+          link: 'https://juejin.cn/post/6963433125808455716',
         },
         {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
+          title: '27岁转行风雨路3——破局35岁，程序猿转型之路',
+          link: 'https://juejin.cn/post/6968719664897589279',
         },
       ],
       list2: [
         {
-          title: '',
-          link: '',
+          title: 'Javascript——面向对象程序设计和继承',
+          link: 'http://www.dadafastrun.com/blog/JS/object.html#%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F',
         },
         {
-          title: '',
-          link: '',
+          title: 'ReactNative技术栈总结',
+          link: 'http://www.dadafastrun.com/blog/app/RNSummary.html',
         },
         {
-          title: '',
-          link: '',
+          title: 'HTTP 协议总结',
+          link: 'http://www.dadafastrun.com/blog/web/http.html',
         },
         {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
-        },
-        {
-          title: '',
-          link: '',
+          title: 'docker学习笔记',
+          link: 'http://www.dadafastrun.com/blog/goujian/docker.html',
         },
       ],
     };
@@ -124,6 +80,7 @@ export default {
   padding-bottom: 80px;
   .blog-wrap {
     .flex-row;
+    align-items: flex-start;
     margin-top: 40px;
     .blog-item {
       width: 450px;
@@ -133,13 +90,20 @@ export default {
       h4 {
         font-size: 20px;
         font-weight: bold;
-        border-bottom: 1px solid #000;
+        border-bottom: 1px solid #e1e1e1;
+        padding-bottom: 10px;
       }
       .list-wrap {
         .list-item {
           border-bottom: 1px solid #e1e1e1;
           font-size: 16px;
           padding: 10px 0;
+          a {
+            color: #333;
+            &:hover {
+              color: @primary-color;
+            }
+          }
         }
       }
     }
