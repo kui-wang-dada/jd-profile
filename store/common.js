71742b53ba77a33b3967b80e11e9dd08deebf85d
wangkui <872505550@qq.com>
2022-04-24 11:23:17 +0800

    new

--- store/common.js
+++ store/common.js
@@ -1,14 +1,9 @@
 /** @format */
-import { typeList, teamList, newsType, serviceList } from '@/assets/mock/common';
 import { tabList, projectList } from '@/utils';
 export default {
   state: () => ({
     tabList,
     projectList,
-    typeList,
-    teamList,
-    newsType,
-    serviceList,
   }),
   mutations: {
     // 设置用户信息
@@ -17,18 +12,6 @@ export default {
     },
     SET_PROJECT_LIST: (state, data) => {
       state.projectList = data;
-    },
-    SET_TYPE_LIST: (state, data) => {
-      state.typeList = data;
-    },
-    SET_TEAM_LIST: (state, data) => {
-      state.teamList = data;
-    },
-    SET_NEWS_TYPE: (state, data) => {
-      state.newsType = data;
-    },
-    SET_SERVICE_LIST: (state, data) => {
-      state.serviceList = data;
     },
   },
   actions: {},
